8ba6d5332ffe9d875c6cef03d0c96a78f1cc3c60
Dipesh Chaudhari <chaudhari.dipesh3360@gmail.com>
2021-01-25 01:17:40 +0530

    User authentication

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './Login.css';
+import { auth } from "./firebase";
 
 function Login() {
+
+    const history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e =>{
+        e.preventDefault();
+        //firbase goes here for signin
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e =>{
+        e.preventDefault();
+        //firbase stuff for register
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+              //it successfully created a new user with email and password 
+              
+              if(auth){
+                  history.push('/')
+              }
+            })
+            .catch(error => alert(error.message))
+
+
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -18,17 +53,22 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text"/>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type="password"/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button
+                    type="submit"
+                     onClick={signIn}  className="login__signInButton">Sign In</button>
                 </form>
                 <p>
                     By signing-in you agree to Amazon's Conditions of use & Sale. Please see our privacy Notice, our Cookies Notice and Intrest-based Ads Notice.
                 </p>
-                <button className="login__registerButton">Create your Amazon Account</button>
+                <button 
+                type="submit"
+                onClick={register}
+                className="login__registerButton">Create your Amazon Account</button>
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from "firebase";
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBZB8e4c2eccJ9LLR859OO4wgA0pts6q2E",
     authDomain: "clone-329df.firebaseapp.com",
@@ -8,3 +13,10 @@ const firebaseConfig = {
     appId: "1:949441761953:web:42ae85ef24b94d11e6d6e0",
     measurementId: "G-B8PK8ELH9X"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
